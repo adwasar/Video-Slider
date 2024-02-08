17e54f7e40a73805555e6986863d53c7c1c98398
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-02-08 17:17:19 +0200

    feat: scroll slider

--- src/script.js
+++ src/script.js
@@ -1,9 +1,31 @@
 const sliderList = document.querySelector('.slider-list')
-const mockList = [{}, {}, {}, {}, {}, {}, {}, {}]
+const btnBack = document.querySelector('.btn-back')
+const btnNext = document.querySelector('.btn-next')
 
-mockList.forEach((_el, i) => {
+const mockList = [{}, {}, {}, {}, {}, {}, {}, {}]
+const sliderStep = 212
+
+let stepIndex = 0
+
+mockList.forEach(() => {
   const listItem = document.createElement('li')
   listItem.classList.add('list-item')
   listItem.innerText = 'loading...'
   sliderList.appendChild(listItem)
+})
+
+btnBack.addEventListener('click', () => {
+  if (stepIndex > 0) {
+    stepIndex--
+    sliderList.style.transform = `translateX(${sliderStep * stepIndex * -1}px)`
+  }
+})
+
+btnNext.addEventListener('click', () => {
+  const elAmount = document.querySelectorAll('.list-item').length
+
+  if (stepIndex < elAmount - 4) {
+    stepIndex++
+    sliderList.style.transform = `translateX(${sliderStep * stepIndex * -1}px)`
+  }
 })
