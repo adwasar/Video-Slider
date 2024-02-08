4f483df5904bf07be36cc66bc2f362ffdf12681a
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-02-08 18:48:14 +0200

    feat: set prewiews

--- src/script.js
+++ src/script.js
@@ -2,16 +2,47 @@ const sliderList = document.querySelector('.slider-list')
 const btnBack = document.querySelector('.btn-back')
 const btnNext = document.querySelector('.btn-next')
 
-const mockList = [{}, {}, {}, {}, {}, {}, {}, {}]
-const sliderStep = 212
+const tokenKay = 'd2377b8aff3d70561aa2fb1e289397ce'
+const videoIds = [
+  '402032394',
+  '403530213',
+  '224199451',
+  '404407889',
+  '908308173',
+  '404648675',
+  '405247867',
+  '399656053',
+]
+const sliderStep = 228
 
 let stepIndex = 0
 
-mockList.forEach(() => {
-  const listItem = document.createElement('li')
-  listItem.classList.add('list-item')
-  listItem.innerText = 'loading...'
-  sliderList.appendChild(listItem)
+videoIds.forEach((id) => {
+  fetch(`https://api.vimeo.com/videos/${id}`, {
+    headers: {
+      Authorization: `Bearer ${tokenKay}`,
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Request is failed with status ${response.status}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      if (data.pictures) {
+        const listItem = document.createElement('li')
+        listItem.classList.add('list-item')
+        sliderList.appendChild(listItem)
+
+        const preview = document.createElement('img')
+        preview.src = data.pictures.sizes[3].link
+        listItem.appendChild(preview)
+      }
+
+      console.log(data.pictures.sizes[2])
+    })
+    .catch((err) => console.log(err.message))
 })
 
 btnBack.addEventListener('click', () => {
